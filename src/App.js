6d6724dd89a77f3b3d608eb213e910cde34ef758
Fatoumata86 <soukounafatoumata86@gmail.com>
2021-08-17 21:39:30 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import "./users.json";
 
-function App() {
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		const users =[];
+		for (let i = 0; i > users.length; i++) {
+			users.push({
+				
+			})
+			console.log(users)
+		}
+		this.state = {users};
+	}
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {this.state.users.map((users) => 
+		<div>
+		 <p>Nom : {users.name}</p>
+		 <p>Email : {users.email}</p>
+		 <p>Website : {users.website}</p>
+		 </div>
+	)}
+    </>
+  )
+}
 }
 
 export default App;
